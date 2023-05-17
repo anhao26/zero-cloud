e4f726944bd2a04b1fa1edcaac75528ad4ecc328
卸逸 <anhao1226@163.com>
2023-05-17 15:07:15 +0800

    添加rpc

---
+++ system/system-rpc/internal/logic/student/delete_student_logic.go
@@ -0,0 +1,37 @@
+package student
+
+import (
+	"context"
+
+    "github.com/anhao26/zero-cloud/system/system-rpc/ent/student"
+    "github.com/anhao26/zero-cloud/system/system-rpc/internal/svc"
+    "github.com/anhao26/zero-cloud/system/system-rpc/internal/utils/dberrorhandler"
+    "github.com/anhao26/zero-cloud/system/system-rpc/types/system"
+
+    "github.com/suyuan32/simple-admin-common/i18n"
+    "github.com/zeromicro/go-zero/core/logx"
+)
+
+type DeleteStudentLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewDeleteStudentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DeleteStudentLogic {
+	return &DeleteStudentLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *DeleteStudentLogic) DeleteStudent(in *system.IDsReq) (*system.BaseResp, error) {
+	_, err := l.svcCtx.DB.Student.Delete().Where(student.IDIn(in.Ids...)).Exec(l.ctx)
+
+    if err != nil {
+		return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
+	}
+
+    return &system.BaseResp{Msg: i18n.DeleteSuccess}, nil
+}
---
+++ system/system-rpc/internal/logic/student/get_student_by_id_logic.go
@@ -0,0 +1,56 @@
+package student
+
+import (
+	"context"
+
+	"github.com/anhao26/zero-cloud/system/system-rpc/internal/svc"
+	"github.com/anhao26/zero-cloud/system/system-rpc/internal/utils/dberrorhandler"
+	"github.com/anhao26/zero-cloud/system/system-rpc/types/system"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetStudentByIdLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetStudentByIdLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetStudentByIdLogic {
+	return &GetStudentByIdLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetStudentByIdLogic) GetStudentById(in *system.IDReq) (*system.StudentInfo, error) {
+	result, err := l.svcCtx.DB.Student.Get(l.ctx, in.Id)
+	if err != nil {
+		return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
+	}
+
+	return &system.StudentInfo{
+		Id:          result.ID,
+		CreatedAt:   result.CreatedAt.UnixMilli(),
+		UpdatedAt:   result.UpdatedAt.UnixMilli(),
+			Name:	result.Name,
+			Age:	int64(result.Age),
+			AgeInt8:	int32(result.AgeInt8),
+			AgeUint8:	uint32(result.AgeUint8),
+			AgeInt16:	int32(result.AgeInt16),
+			AgeUint16:	uint32(result.AgeUint16),
+			AgeInt32:	result.AgeInt32,
+			AgeUint32:	result.AgeUint32,
+			AgeInt64:	result.AgeInt64,
+			AgeUint64:	result.AgeUint64,
+			AgeInt:	int64(result.AgeInt),
+			AgeUint:	uint64(result.AgeUint),
+			WeightFloat:	result.WeightFloat,
+			WeightFloat32:	result.WeightFloat32,
+			ClassId:	result.ClassID.String(),
+			EnrollAt:	result.EnrollAt.UnixMilli(),
+			StatusBool:	result.StatusBool,
+	}, nil
+}
+
